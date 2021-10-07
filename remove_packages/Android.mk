88cc921c2ca195a50840994e40b1f4bdd92df4a3
Manikantaraavi <raavimanikanta6595@gmail.com>
2021-10-07 08:46:16 +0000

    lemonkebab: remove more packages
    Signed-off-by: Manikantaraavi <raavimanikanta6595@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AudioFX MusicFX NfcNci Photos Snap
+LOCAL_OVERRIDES_PACKAGES := AudioFX MusicFX NfcNci Photos Snap Camera2 GalleryGoPrebuilt Gallery
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
